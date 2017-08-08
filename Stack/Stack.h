ad7c1e7d73847fba1e39d87530b57e8d51a363e7
Marius Ilie <marius.ilie@mready.net>
2017-08-09 00:37:55 +0300

    Initial Commit

---
+++ Stack/Stack.h
@@ -0,0 +1,13 @@
+//
+//  Stack.h
+//  Stack
+//
+//  Created by Marius Ilie on 09/08/2017.
+//  Copyright © 2017 Marius Ilie. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Stack : NSObject
+
+@end
---
+++ Stack/Stack.m
@@ -0,0 +1,13 @@
+//
+//  Stack.m
+//  Stack
+//
+//  Created by Marius Ilie on 09/08/2017.
+//  Copyright © 2017 Marius Ilie. All rights reserved.
+//
+
+#import "Stack.h"
+
+@implementation Stack
+
+@end
